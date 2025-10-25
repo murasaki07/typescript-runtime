178b0f1aba43c06f9b5394e23d476457dce432bf
MuraSaki07 <91040927+MuraSaki07@users.noreply.github.com>
2025-10-25 22:46:33 +0900

    save

--- src/lib/azureOpenAI.ts
+++ src/lib/azureOpenAI.ts
@@ -1,11 +1,14 @@
-import { AzureOpenAI } from 'openai'
+import { OpenAI } from 'openai'
 import 'dotenv/config'
 const apiKey = process.env['AZURE_OPENAI_API_KEY']
-const apiVersion = process.env['AZURE_OPENAI_API_VERSION']
+const apiVersion =
+  process.env['AZURE_OPENAI_API_VERSION'] || '2025-03-01-preview'
 const endpoint = process.env['AZURE_OPENAI_ENDPOINT']
 console.log('🚀 ~ endpoint:', endpoint)
-export const azureOpenAIClient = new AzureOpenAI({
+console.log('🚀 ~ apiVersion:', apiVersion)
+export const azureOpenAIClient = new OpenAI({
   apiKey,
-  apiVersion,
-  baseURL: endpoint,
+  baseURL: `${endpoint}/openai`,
+  defaultQuery: { 'api-version': apiVersion },
+  defaultHeaders: { 'api-key': apiKey },
 })
